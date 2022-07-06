8dd76cef3e6bc20a74787bd32fc86ac053d7e6de
KKanghh <84486674+KKanghh@users.noreply.github.com>
2022-07-06 15:31:05 +0900

    [Silver II] Title: 종이의 개수, Time: 1076 ms, Memory: 20704 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int board[2187][2187];
+int cnt[3];
+
+bool check(int a, int b, int n) {
+	for (int i = a; i < a + n; i++) {
+		for (int j = b; j < b + n; j++) {
+			if (board[a][b] != board[i][j]) return false;
+		}
+	}
+	return true;
+}
+void paper(int a, int b, int n) {
+	if (check(a, b, n)) {
+		cnt[board[a][b] + 1]++;
+		return;
+	}
+	int k = n / 3;
+	for (int i = a; i < a + n; i += k) {
+		for (int j = b; j < b + n; j += k) {
+			paper(i, j, k);
+		}
+	}
+
+}
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> board[i][j];
+		}
+	}
+	paper(0, 0, n);
+	for (int i = 0; i < 3; i++) cout << cnt[i] << '\n';
+}
