296428a840f27830977e5d7560c0d29e65bafacf
KKanghh <84486674+KKanghh@users.noreply.github.com>
2022-07-10 20:47:08 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, num[10], arr[10];
+void func(int k, int t) {
+    if (k == m) {
+        for (int i = 0; i < m; i++) cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    int tmp = 0;
+    for (int i = t; i < n; i++) {
+        if (num[i] == tmp) continue;
+        arr[k] = num[i];
+        tmp = num[i];
+        func(k + 1, i + 1);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) cin >> num[i];
+    sort(num, num + n);
+    func(0, 0);
+}
