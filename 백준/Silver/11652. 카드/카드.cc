5a1eaaaa7db657a6f988237b0f154eab9a4a440e
KKanghh <84486674+KKanghh@users.noreply.github.com>
2022-08-03 15:42:43 +0900

    [Silver IV] Title: 카드, Time: 28 ms, Memory: 3684 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+
+vector<long long> V;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		long long num;
+		cin >> num;
+		V.push_back(num);
+	}
+
+	sort(V.begin(), V.end());
+	int cnt = 0;
+	int mx_cnt = 0;
+	long long mx_num;
+	for (int i = 0; i < V.size(); i++) {
+		if (i == 0 || V[i] == V[i - 1]) cnt++;
+		else cnt = 1;
+		if (cnt > mx_cnt) {
+			mx_cnt = cnt;
+			mx_num = V[i];
+		}
+		else if (cnt == mx_cnt) {
+			mx_num = min(V[i - 1], mx_num);
+		}
+	}
+
+	cout << mx_num;
+}
