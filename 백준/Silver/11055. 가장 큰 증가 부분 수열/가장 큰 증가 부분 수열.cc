8a4d7fd5205e35184b3cd9c9e2742154fb6c2c54
KKanghh <84486674+KKanghh@users.noreply.github.com>
2022-08-07 17:16:09 +0900

    [Silver II] Title: 가장 큰 증가 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가 부분 수열/가장 큰 증가 부분 수열.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int A[1001], D[1001];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> A[i];
+		D[i] = A[i];
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j < i; j++) {
+			if (A[j] < A[i]) D[i] = max(D[i], D[j] + A[i]);
+		}
+	}
+
+	cout << *max_element(D + 1, D + n + 1);
+	
+}
