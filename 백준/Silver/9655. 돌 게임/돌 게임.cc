99a94324ae67e6dbbd75d70cd51dd26fd57acbd8
KKanghh <84486674+KKanghh@users.noreply.github.com>
2022-08-16 15:25:57 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.cc
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -1,18 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool D[1001];
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	int n;
 	cin >> n;
-	D[1] = true;
 
-	for (int i = 2; i <= n; i++) {
-		D[i] = !D[i - 1];
-	}
-
-	if (D[n]) cout << "SK";
+	if (n % 2 == 1) cout << "SK";
 	else cout << "CY";
 }
