ed332ea607cdb2d37fd4f8c383824cf654ca1562
KKanghh <84486674+KKanghh@users.noreply.github.com>
2022-08-24 18:52:40 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,37 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int num[100000];
-int N, M;
+int arr[100000], n;
+
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> N;
-    
-    for (int i = 0; i < N; i++) {
-        cin >> num[i];
-    }
-    sort(num, num + N);
-    cin >> M;
-    
-    while (M--) {
-        int temp;
-        cin >> temp;
-        int low = 0;
-        int high = N - 1;
-        bool find = false;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (num[mid] < temp) low = mid + 1;
-            else if (num[mid] > temp) high = mid - 1;
-            else {
-                find = true;
-                break;
-            }
-        }
-        
-        if (find) cout << 1 << '\n';
-        else cout << 0 << '\n';
-    }
-    
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + n);
+
+	int m;
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		int k;
+		cin >> k;
+		cout << binary_search(arr, arr + n, k) << '\n';
+	}
 }
