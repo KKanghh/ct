31b2e90e0fd2ce521e338e75b8a886f56f767816
KKanghh <84486674+KKanghh@users.noreply.github.com>
2022-09-14 20:07:43 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 88 ms, Memory: 12884 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+unordered_set<string> S;
+
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	set<string> S;
 	int n;
 	cin >> n;
 	while (n--) {
@@ -17,9 +18,8 @@ int main() {
 			S.insert(name);
 		}
 	}
-	auto e = --S.end();
-	while (e != S.begin()) cout << *e-- << '\n';
-	//sort(S.begin(), S.end());
-	cout << *e;
-	//for (string s : S) cout << s << '\n';
+	vector<string> V(S.begin(), S.end());
+	sort(V.begin(), V.end(), greater <string>());
+	for (string e : V) cout << e << '\n';
+
 }
