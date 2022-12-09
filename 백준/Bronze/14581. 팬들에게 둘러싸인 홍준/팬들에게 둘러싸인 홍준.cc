b8ebcde5f5f6858fef22f03a8989b72547cf9888
KKanghh <84486674+KKanghh@users.noreply.github.com>
2022-12-09 16:01:24 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string name;
+	cin >> name;
+
+	cout << ":fan::fan::fan:\n:fan::" << name << "::fan:\n:fan::fan::fan:";
+}
