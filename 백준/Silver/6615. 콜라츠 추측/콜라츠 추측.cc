1fc61f0971561721d2e831eeb405c93f3a27211b
KKanghh <84486674+KKanghh@users.noreply.github.com>
2023-01-27 14:00:39 +0900

    [Silver III] Title: 콜라츠 추측, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/6615. 콜라츠 추측/콜라츠 추측.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> A, B;
+
+long long md(long long num) {
+	if (num % 2) return 3 * num + 1;
+	return num / 2;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	while (true) {
+		long long a, b;
+		
+		cin >> a >> b;
+
+		if (a == 0 && b == 0) break;
+
+		long long aa = a, bb = b;
+
+		while (true) {
+			A.push_back(a);
+			if (a == 1) break;
+			a = md(a);
+		}
+
+		while (true) {
+			B.push_back(b);
+			if (b == 1) break;
+			b = md(b);
+		}
+
+		int lenA = A.size();
+		int lenB = B.size();
+		int minLen = min(lenA, lenB);
+		int i, p;
+		for (i = 0; i < minLen; i++) {
+			if (A[lenA - i - 1] != B[lenB - i - 1]) break;
+			p = A[lenA - i - 1];
+		}
+
+		cout << aa << " needs " << lenA - i << " steps, " << bb << " needs " << lenB - i << " steps, they meet at " << p << '\n';
+		A.erase(A.begin(), A.end());
+		B.erase(B.begin(), B.end());
+	}
+}
