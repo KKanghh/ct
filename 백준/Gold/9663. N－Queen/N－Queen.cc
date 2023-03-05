3976e3239bff063fdf2aec886fc34dd70b3f19d4
KKanghh <84486674+KKanghh@users.noreply.github.com>
2023-03-05 21:34:11 +0900

    [Gold IV] Title: N-Queen, Time: 6960 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, cnt;
+bool board[15][15];
+
+bool check(int x, int y) {
+	int i = x, j = y;
+	while (i >= 0 && j >= 0) {
+		if (board[i][j]) return false;
+		i--; j--;
+	}
+	i = x, j = y;
+	while (i >= 0 && j < n) {
+		if (board[i][j]) return false;
+		i--; j++;
+	}
+	i = x, j = y;
+	while (i >= 0) {
+		if (board[i][j]) return false;
+		i--;
+	}
+	return true;
+}
+
+void func(int k) {
+	if (k == n) {
+		cnt++;
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+		if (check(k, i)) {
+			board[k][i] = true;
+			func(k + 1);
+			board[k][i] = false;
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n;
+	func(0);
+	cout << cnt;
+}
