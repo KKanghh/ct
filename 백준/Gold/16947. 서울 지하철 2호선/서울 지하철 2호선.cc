809ab0b4d1876ae7666234c2a7da640ef3c0f4dc
KKanghh <84486674+KKanghh@users.noreply.github.com>
2023-04-12 15:04:40 +0900

    [Gold III] Title: 서울 지하철 2호선, Time: 168 ms, Memory: 2252 KB -BaekjoonHub

--- 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.cc
+++ 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.cc
@@ -1,44 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> adj[3001];
-int dist[3001];
-bool loop;
-int mn;
-
-void dfs(int a, int b) {
-	for (int e : adj[b]) {
-		if (e == a) continue;
-
-		if (dist[e] >= 0) {
-			mn = min(mn, dist[e]);
-		}
-		else {
-			dist[e] = dist[b] + 1;
-			dfs(b, e);
-		}
-	}
-}
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	int n;
-	cin >> n;
-
-	for (int i = 1; i <= n; i++) {
-		int a, b;
-		cin >> a >> b;
-		adj[a].push_back(b);
-		adj[b].push_back(a);
-	}
-
-	for (int i = 1; i <= n; i++) {
-		loop = false;
-		mn = 0x7f7f7f7f;
-		fill(dist, dist + n + 1, -1);
-		dist[i] = 0;
-		dfs(0, i);
-		cout << mn << ' ';
-	}
-}
+vector<int> j[3001];
+int d[3001],mn,n,a,b,i;
+void c(int a,int b){for(int e:j[b]){if(e==a){continue;}if(d[e]>=0){mn=min(mn,d[e]);}else{d[e]=d[b]+1;c(b,e);}}}
+int main(){ios::sync_with_stdio(0);cin.tie(0);cin>>n;for(i=1;i<=n;i++){cin>>a>>b;j[a].push_back(b);j[b].push_back(a);}for(i=1;i<=n;i++){mn=3002;fill(d,d+n+1,-1);d[i]=0;c(0,i);cout<<mn<<' ';}}
