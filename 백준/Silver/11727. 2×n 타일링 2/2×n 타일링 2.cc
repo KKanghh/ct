cef20ff83724b8fffa6b6f80f4164479ee8d415b
Kang Hwanhee <84486674+KKanghh@users.noreply.github.com>
2023-05-04 15:07:22 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -5,14 +5,14 @@ int D[1001];
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	D[1] = 1;
-	D[2] = 3;
+
 	int n;
 	cin >> n;
-	for (int i = 3; i <= n; i++) {
-		D[i] = (D[i - 1] + 2 * D[i - 2]) % 10007;
-	}
+
+	D[0] = 1;
+	D[1] = 1;
+	
+	for (int i = 2; i <= n; i++) D[i] = (D[i - 1] + D[i - 2] * 2) % 10007;
 
 	cout << D[n];
-
 }
