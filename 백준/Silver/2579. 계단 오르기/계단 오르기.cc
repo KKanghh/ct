e2b79f12926f60f86187569d1187bc2e12def8b4
Kang Hwanhee <84486674+KKanghh@users.noreply.github.com>
2023-05-09 15:16:05 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,23 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+int stair[301];
+int D[301][2];
 
-int stair[301], ans[2][301];
-int N;
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> N;
-	for (int i = 1; i <= N; i++) {
-		cin >> stair[i];
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) cin >> stair[i];
+
+	D[1][0] = stair[1];
+
+	for (int i = 2; i <= n; i++) {
+		D[i][0] = max(D[i - 2][0], D[i - 2][1]) + stair[i];
+		D[i][1] = D[i - 1][0] + stair[i];
 	}
 
-	ans[0][1] = stair[1];
-    ans[1][1] = stair[1];
-	for (int i = 2; i <= N; i++) {
-		ans[0][i] = ans[1][i - 1] + stair[i];
-		ans[1][i] = max(ans[0][i - 2], ans[1][i - 2]) + stair[i];
-	}
-
-	cout << max(ans[0][N], ans[1][N]);
+	cout << max(D[n][0], D[n][1]);
 }
