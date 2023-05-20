2605f00b9c900b8111fd6e5b92390b3b6f784729
Kang Hwanhee <84486674+KKanghh@users.noreply.github.com>
2023-05-21 01:40:16 +0900

    [Gold V] Title: 암호코드, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Gold/2011. 암호코드/암호코드.cc
+++ 백준/Gold/2011. 암호코드/암호코드.cc
@@ -1,51 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long D[5001];
-string a;
-vector<int> V;
+int D[5001], num[5001];
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> a;
-	int len = a.length();
-	if (a[0] == '0') {
-		cout << 0;
-		return 0;
+	string s;
+	cin >> s;
+	int n = s.size();
+	
+	for (int i = 0; i < n; i++) {
+		num[i + 1] = s[i] - '0';
 	}
+
 	D[0] = 1;
-	D[1] = 1;
-	for (int i = 2; i <= len; i++) D[i] = (D[i - 2] + D[i - 1]) % 1000000;
 
-	V.push_back(1);
-	int pos = 0;
-	for (int i = 1; i < len; i++) {
-		if (a[i] == '0') {
-			if (a[i - 1] == '0' || a[i - 1] - '0' > 2) {
-				cout << 0;
-				return 0;
-			}
-			V[pos]--;
-			V.push_back(1);
-		}
-		else if (a[i - 1] - '0' == 1) {
-			V[pos]++;
-		}
-		else if (a[i - 1] - '0' == 2 && a[i] - '0' <= 6) {
-			V[pos]++;
-		}
-		else {
-			V.push_back(1);
-			pos++;
-		}
+	for (int i = 1; i <= n; i++) {
+		if (num[i] > 0) D[i] = D[i - 1] % 1000000;
+        int t = num[i - 1] * 10 + num[i];
+		if (t >= 10 && t <= 26) D[i] = (D[i] + D[i - 2]) % 1000000;
 	}
 
-	int cnt = 1;
-
-	for (int e : V) {
-		cnt = (cnt * D[e]) % 1000000;
-	}
-
-	cout << cnt;
+	cout << D[n];
 }
