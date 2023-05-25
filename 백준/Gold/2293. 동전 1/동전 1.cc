901dc8de5ca4abd0b169f2c2ae74e79e8d165532
Kang Hwanhee <84486674+KKanghh@users.noreply.github.com>
2023-05-25 16:55:40 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.cc
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -1,23 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a[101], D[10001];
+int D[10001];
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	
-	int n, m;
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-	}
+	int n, k;
+	cin >> n >> k;
 	D[0] = 1;
+
 	for (int i = 0; i < n; i++) {
-		for (int j = a[i]; j <= m; j++) {
-			D[j] += D[j - a[i]];
-		}
+		int t;
+		cin >> t;
+		for (int j = t; j <= k; j++) D[j] += D[j - t];
 	}
-	
-	cout << D[m];
+
+	cout << D[k];
 }
