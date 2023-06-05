4052104022e6860cb744c9c7dfdf67eaa22696ee
Kang Hwanhee <84486674+KKanghh@users.noreply.github.com>
2023-06-05 16:39:06 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,21 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+int cnt[26];
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    string input;
-    int result[26];
-    
-    fill(result, result + 26, 0);
-    cin >> input;
-    
-    for (int i = 0; i < input.size(); i++) {
-        result[input[i] - 'a']++;
-    }
-    
-    for (int i = 0; i < 26; i++) cout << result[i] << ' ';
-    
-    
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string s;
+	cin >> s;
+
+	for (int i = 0; i < s.size(); i++) {
+		cnt[s[i] - 'a']++;
+	}
+
+	for (int i = 0; i < 26; i++) cout << cnt[i] << ' ';
 }
