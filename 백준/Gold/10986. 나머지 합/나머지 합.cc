117ec4cb3e55cc65ae131ce99806d2125fdd55e7
KKanghh <attsky2@gmail.com>
2023-12-03 18:19:25 +0900

    [Gold III] Title: 나머지 합, Time: 136 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.cc
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -6,14 +6,14 @@ int cnt[1000];
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n, m, z = 0;
+    int n, m = 0;
+    cnt[0] = 1;
     long long ans = 0;
     cin >> n >> m;
 
     for (int i = 0; i < n; i++) {
         cin >> num[i + 1];
         D[i + 1] = (D[i] + (num[i + 1] % m)) % m;
-        if (D[i + 1] == 0) ans++; 
         ans += cnt[D[i + 1]];
         cnt[D[i + 1]]++;
     }
